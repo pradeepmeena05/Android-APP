5d14e966d8f8ae97e8a874b3fcad609a33947874
pradeepmeena05 <pradeepmeena34330@gmail.com>
2025-01-25 15:33:17 +0530

    framelayout added

---
+++ app/src/main/java/framelayout/FrameActivity.java
@@ -0,0 +1,17 @@
+package framelayout;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.myapplication.R;
+
+public class FrameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_frame);
+
+    }
+}
