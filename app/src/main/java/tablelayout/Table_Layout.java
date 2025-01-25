a731e1dce6e22ea00a2ba9eacbbf90467ef9b911
pradeepmeena05 <pradeepmeena34330@gmail.com>
2025-01-25 17:37:37 +0530

    Table Layout added successfully

--- app/src/main/java/tablelayout/Table_Layout.java
+++ app/src/main/java/tablelayout/Table_Layout.java
@@ -2,11 +2,7 @@ package tablelayout;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.example.myapplication.R;
 
@@ -15,12 +11,7 @@ public class Table_Layout extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_table_layout);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
     }
 }
