beac3a86517483d5092d07a1261c288a1045c439
pradeepmeena05 <pradeepmeena34330@gmail.com>
2025-01-25 14:48:07 +0530

    gradle updated git add .
    searchview
    git commit -m "gradle updated"

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.myapplication"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.unitmyapplication"
@@ -32,11 +32,19 @@ android {
 }
 
 dependencies {
+//    implementation ("com.android.volley:volley:1.2.1")
+//    implementation ("com.squareup.picasso:picasso:2.71828")
+
+
+    implementation ("androidx.recyclerview:recyclerview:1.3.1")
+    implementation ("androidx.cardview:cardview:1.0.0")
+
 
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(files("/home/rahul/AndroidStudioProjects/Android/app/app/lib/json-20140107.jar"))
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
